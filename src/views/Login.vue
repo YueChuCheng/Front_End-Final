c8d02c1bbf77650b20ecaa4a685117f9eb3f2b99
Yue Chu Cheng <maggie9907@gmail.com>
2019-06-09 22:11:23 +0800

    register page finish

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="container main">
+  <div class="main">
+      
+    <LogoTag/>
     <div class="row">
-      <div class="title_style"></div>
-      <div class="col title">登入</div>
+      <div class="col title">
+        登入
+        <div class="title_style"></div>
+      </div>
     </div>
     <div class="row">
-      <div class="col subtitle mt-3">以社群媒體登入</div>
+      <div class="col subtitle mt-4">以社群媒體登入</div>
     </div>
-    <div class="row d-flex justify-content-center login mt-3">
-      <div class="col-3 googleLogin"></div>
-      <div class="col-3 fbLogin"></div>
+    <div class="row d-flex justify-content-center login mt-4">
+      <div class="col-2 googleLogin"></div>
+      <div class="col-2 fbLogin"></div>
     </div>
-    <div class="row or_style d-flex justify-content-center login align-items-center mt-3">
+    <div class="row or_style d-flex justify-content-center login align-items-center mt-4">
       <div class="col-3 line"></div>
       <div class="col-1 text">或</div>
       <div class="col-3 line"></div>
@@ -27,11 +31,11 @@
       </div>
     </div>
     <div class="row mt-5">
-      <div class="col  d-flex align-items-center justify-content-end">
+      <div class="col d-flex align-items-center justify-content-end">
         <div class="submit">登入</div>
       </div>
-      <div class=" forget_style col  d-flex align-items-center">
-        <a href="" class="forget_style">忘記密碼?</a>
+      <div class="forget_style col d-flex align-items-center">
+        <a href class="forget_style">忘記密碼?</a>
       </div>
     </div>
   </div>
@@ -44,35 +48,51 @@
 * {
   font-family: "Noto Sans TC", sans-serif;
 }
+.logo_style {
+  position: absolute;
+  background-color: #232323;
+  height: 5vw;
+  width: 17vw;
+  margin-left: -6%;
+  margin-top: -4%;
+  border-radius: 10vw;
+}
+img {
+  position: absolute;
+  top: 18%;
+  left: 40%;
+  height: 90%;
+}
 .outline {
   outline: solid 2px red;
 }
 .main {
+  padding-top: 5%;
   min-height: 80vh;
-
 }
 .title {
   color: #262626;
   font-weight: 300;
-  font-size: 50px;
+  font-size: 250%;
 }
 .title_style {
+  z-index: -1;
   position: absolute;
   background: #219e91;
-  left: 48.5%;
-  top: 65px;
-  height: 2rem;
-  width: 6.5rem;
+  margin-left: 47.5%;
+  margin-top: -1.5%;
+  height: 2vw;
+  width: 6vw;
   border-radius: 50px;
 }
 .subtitle {
-  font-size: 21px;
+  font-size: 100%;
 }
 
 .googleLogin {
   cursor: pointer;
   background: #fff;
-  height: 60px;
+  height: 4vw;
   border-radius: 0.8vw;
   border: #7c7c7c solid 0.5px;
   margin-right: 20px;
@@ -80,7 +100,7 @@
 .fbLogin {
   cursor: pointer;
   background: #3b5998;
-  height: 60px;
+  height: 4vw;
   border-radius: 0.8vw;
   margin-left: 20px;
 }
@@ -110,15 +130,21 @@
   border-radius: 10px;
   line-height: 38px;
 }
-a{
-    
-    color:#ff794a !important;
+a {
+  color: #ff794a !important;
 }
 </style>
 
 <script>
+
 import "bootstrap";
-export default {};
+import LogoTag from "@/components/LogoTag.vue";
+export default {
+  name: 'login',
+  components: {
+    LogoTag
+  }
+};
 </script>
 
 
