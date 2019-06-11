3418b96cbc3d0cb732aa8c11699e05893dedce31
Yue Chu Cheng <maggie9907@gmail.com>
2019-06-11 22:27:11 +0800

    add firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+
+let config = {
+  apiKey: "AIzaSyBe2QWeHqJvjkSurqXhqZuU-qqyaOCC4KY",
+  authDomain: "test-firestore-847fa.firebaseapp.com",
+  databaseURL: "https://test-firestore-847fa.firebaseio.com",
+  projectId: "test-firestore-847fa",
+  storageBucket: "test-firestore-847fa.appspot.com",
+  messagingSenderId: "1067319899742",
+  appId: "1:1067319899742:web:8c701210401314f8"
+ 
+}
+
+firebase.initializeApp(config);
+window.firebase=firebase;
+var db = firebase.firestore();
