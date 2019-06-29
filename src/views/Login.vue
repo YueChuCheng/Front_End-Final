e91928c7dc970246ceca892ec793d23d58ac313d
Yue Chu Cheng <maggie9907@gmail.com>
2019-06-30 00:49:14 +0800

    fix css

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main_section">
+  <!-- <div class="main_section">
     <LogoTag/>
 
     <div class="card_deco">
@@ -29,13 +29,13 @@
     <div class="row mt-3">
       <div class="col">
         <input type="text " class="input input1" placeholder="信箱">
-        <!-- <img src="../assets/icon/icon_letter.png" alt="" class="icon_placeh">  -->
+       
       </div>
     </div>
     <div class="row mt-4">
       <div class="col">
         <input type="text " class="input input2" placeholder="密碼">
-        <!-- <img src="../assets/icon/icon_locked.png" alt="" class="icon_placeh"> -->
+      
       </div>
     </div>
     <div class="row mt-5 d-flex align-items-center justify-content-around submit_row">
@@ -51,6 +51,41 @@
         <img src="../assets/picture (2).png" alt class="deco_img">
       </div>
     </div>
+  </div>-->
+  <div class="login">
+    <LogoTag/>
+
+    <div class="login_main">
+      <div class="login_title">
+        <div class="title">登入</div>
+        <div class="title_style"></div>
+      </div>
+      <div class="login_subtitle">以社群媒體登入</div>
+      <div class="login_btn">
+        <div class="google_btn" @click="doLogin">
+          <img src="../assets/icon/google_icon.png" class="icon">
+        </div>
+        <div class="fb_btn">
+          <img src="../assets/icon/icon_fb.png" class="icon">
+        </div>
+      </div>
+      <div class="login_or">
+        <div class="l">
+          <hr>
+        </div>
+        <div class="or">或</div>
+        <div class="l">
+          <hr>
+        </div>
+      </div>
+      <input type="text " class="login_input1" placeholder="信箱">
+      <input type="text " class="login_input2" placeholder="密碼">
+      <div class="login_submitbtn">
+        <button class="submit_btn">登入</button>
+        <div class="forget_btn">忘記密碼</div>
+      </div>
+    </div>
+    <img src="../assets/picture (2).png" alt class="deco_img">
   </div>
 </template>
 
@@ -58,31 +93,22 @@
 <style scoped>
 @import "../css/base.css";
 * {
-  text-align: center;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
   font-family: "Noto Sans TC", sans-serif;
 }
-.icon {
+/* .icon {
   height: 70%;
 }
 h5 {
-  /* writing-mode:vertical-lr; */
+  
   color: #262626;
-  /* border:1px solid #000000; */
+ 
   font-size: 3vw;
   font-weight: 300;
   line-height: 4.25vw;
   position: absolute;
-  width: 3vw;
-}
-.font_bg {
-  width: 3vw;
-  border-radius: 50px;
-  background-color: #219e91;
-  margin-top: 2%;
-  margin-left: 1.25%;
-  color: rgba(0, 0, 0, 0);
-}
-h5 {
   width: 20vw;
 }
 .font_bg {
@@ -90,7 +116,12 @@ h5 {
   height: 2.5vw;
   margin-top: 2.4%;
   margin-left: 8%;
+  border-radius: 50px;
+  background-color: #219e91;
+
+  color: rgba(0, 0, 0, 0);
 }
+
 .card_deco {
   margin-top: 70px;
   margin-left: 39.5%;
@@ -99,7 +130,7 @@ h5 {
   outline: solid 2px red;
 }
 .main_section {
-  padding-top: 7%;
+  padding-top: 40px;
   min-height: 80vh;
   overflow: hidden;
   background-color: #f8f8f8;
@@ -172,7 +203,7 @@ a {
 }
 .deco_img {
   text-align: center;
-   width: 17vw;
+  width: 17vw;
   margin-left: 74vw;
   margin-top: -100px;
 }
@@ -229,9 +260,9 @@ a {
 
 @media screen and (max-width: 480px) {
   h5 {
-    /* writing-mode:vertical-lr; */
+    
     color: #262626;
-    /* border:1px solid #000000; */
+    
     font-size: 5.9vw;
     font-weight: 300;
     line-height: 4.25vw;
@@ -283,6 +314,174 @@ a {
   }
   .subtitle_row {
     margin-top: 150px;
+  }
+} */
+.outline {
+  outline: red solid 3px;
+}
+
+.login {
+  background-color: #f8f8f8;
+  height: 100vh;
+  padding-top: 35px;
+  display: grid;
+  justify-content: center;
+}
+.login_main {
+  margin-top: 10vw;
+  display: grid;
+  grid-template-rows: 40px 40px 40px 40px 40px 55px 70px;
+  justify-items: center;
+  grid-gap: 5px;
+}
+.login_title {
+  display: grid;
+  height: 50px;
+  width: 100px;
+}
+.title {
+  z-index: 10;
+  font-size: 30px;
+  font-weight: 300;
+}
+.title_style {
+  position: relative;
+  background-color: #219e91;
+  height: 24px;
+  width: 75px;
+  border-radius: 50vw;
+  top: -20px;
+  left: 10px;
+}
+.login_subtitle {
+  align-self: center;
+  margin-top: 5px;
+}
+.login_btn {
+  display: grid;
+  width: 430px;
+  grid-template-rows: 100%;
+  grid-template-columns: 200px 200px;
+  justify-content: space-between;
+}
+.google_btn {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border: solid #262626 1px;
+  border-radius: 8px;
+}
+
+.fb_btn {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #3b5998;
+  border: solid #262626 1px;
+  border-radius: 8px;
+}
+.google_btn img {
+  height: 70%;
+}
+.fb_btn img {
+  height: 70%;
+}
+.login_or {
+  display: grid;
+  grid-template-rows: 100%;
+  grid-template-columns: 280px 20px 280px;
+  align-items: center;
+}
+.l {
+  background-color: #262626;
+  height: 1px;
+  width: 100%;
+}
+
+.login_input1 {
+  height: 100%;
+  width: 250px;
+  border-radius: 10px;
+  text-align: left;
+  padding-left: 40px;
+  border: #7c7c7c 0.5px solid;
+  background-image: url(../assets/icon/icon_letter.png);
+  background-repeat: no-repeat;
+  background-size: auto 60%;
+  background-position: 5% 55%;
+}
+
+.login_input2 {
+  height: 40px;
+  width: 250px;
+  border-radius: 10px;
+  text-align: left;
+  padding-left: 40px;
+  border: #7c7c7c 0.5px solid;
+  background-image: url(../assets/icon/icon_locked.png);
+  background-repeat: no-repeat;
+  background-size: auto 60%;
+  background-position: 5% 55%;
+  align-self: end;
+}
+.login_submitbtn {
+  align-self: end;
+  display: grid;
+  width: 260px;
+  grid-template-rows: 100%;
+  grid-template-columns: 120px 120px;
+  justify-content: space-between;
+}
+.submit_btn {
+  background-color: #ff794a;
+  border: none;
+  border-radius: 50vw;
+  color: #fff;
+  height: 40px;
+}
+
+.forget_btn {
+  text-align: center;
+  line-height: 40px;
+  color: #ff794a;
+}
+.deco_img {
+  text-align: center;
+  width: 17vw;
+  margin-left: 74vw;
+  margin-top: -100px;
+}
+@media screen and (max-width: 768px) {
+  .login_main {
+    margin-top: 15vw;
+  }
+  .login_or {
+    grid-template-columns: 38vw 2vw 38vw;
+  }
+  .login_btn {
+    width: 348px;
+    grid-template-columns: repeat(2, 160px);
+  }
+   .deco_img {
+    margin-left: 70%;
+  }
+}
+@media screen and (max-width: 480px) {
+  .login_main {
+    margin-top: 25vw;
+  }
+  .login_btn {
+    display: grid;
+    width: 280px;
+    grid-template-rows: 100%;
+    grid-template-columns: 120px 120px;
+    justify-content: space-between;
+  }
+  .deco_img {
+    display: none;
   }
 }
 </style>
